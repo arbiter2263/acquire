824523832501fbd4c90a3dfcc56ceee1c7ebeb6b
Chris <chrisblaser@isu.edu>
2022-04-03 16:27:07 -0600

    initial coding of tile/pile classes, stock deleted

--- app/src/main/java/acquire/Pile.java
+++ app/src/main/java/acquire/Pile.java
@@ -1,15 +1,102 @@
 package acquire;
 
-import java.util.LinkedList;
+/*
+Pile class holds the required methods to be the un-played tiles.
+This class generates all 108 tiles and shuffles them into a list
+Tiles can be taken and added back into this "pile"
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Pile {
-    private LinkedList<Tile> availableTiles;
-    protected Pile(){
-        //initialize pile
-        //make this class a singleton
+    private static Pile instance = null;
+    private final ArrayList<Tile> pile;
+
+    //Singleton design attempt
+    public static Pile getInstance(){
+        if (instance == null){
+            instance = new Pile();
+        }
+        return instance;
     }
-    protected void destroyTile(Tile tile){}
-    protected Tile drawTile() {
-        return availableTiles.remove();
+
+    /*
+    Constructor
+    initiates the pile as arraylist to hold all 108 tiles
+    adds those tiles to the list
+     */
+    private Pile(){
+
+        pile = new ArrayList<Tile>(108);
+
+        //char array for tile lettering
+        char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'};
+
+        /*
+         loop to add all 108 tiles to the pile initially listing Letter then number value
+         of the tile
+         */
+        for(int i = 1; i <= 9; i++){
+            for(var letter : letters){
+                Tile tile = new Tile(i, letter);
+                pile.add(tile);
+            }
+        //After all are added, shuffle the deck
+        } shuffle();
+    }
+
+    /*
+    Shuffle method for the pile when initializing it.
+    Helps to keep each game a little more random
+     */
+    public void shuffle(){
+        Collections.shuffle(pile);
+    }
+
+    /*
+    In case a tile needs to be returned to the pile
+    @param tile The chosen tile to add back to the pile of tiles
+     */
+    protected void returnTile(Tile tile){
+        pile.add(tile);
+    }
+
+    /*
+    Pile size check method
+    helps to maintain integrity of pile size when
+    removing tiles from
+     */
+    protected int size(){
+        return pile.size();
+    }
+    public String toString() {
+        String sb = "";
+        for (var tile : pile) {
+            sb += tile.getNumber() + " " + tile.getLetter() + "\n";
+        }
+        return sb;
+    }
+    /*
+    method for randomly choosing a tile drawn by the player, method
+    removes tile from pile
+    */
+    public Tile drawTile(){
+
+        /* Randomly chooses number between 0 and size of pile
+           To get a randomly chosen tile.
+        */
+        Random random = new Random();
+        int randomInt = random.nextInt(pile.size());
+        if(pile.size() !=0){
+            return pile.remove(randomInt);
+            //Print out remaining number of tiles?
+        }else if(pile.size() < 1){
+            // TODO: 4/3/2022  add print statement?
+            //print out no tiles left in pile
+            System.out.println("No tiles left to draw");
+        }
+        return null;
     }
 }
--- app/src/main/java/acquire/Tile.java
+++ app/src/main/java/acquire/Tile.java
@@ -1,13 +1,54 @@
 package acquire;
+/*
+Tile class holds the pertinent information related to
+the tiles in the game. Each tile created will have a letter
+and number associated to it, which makes of the space it would
+take on the game board
+ */
+
 
 public class Tile implements TileInterface{
-    private String space;
+    private final String space;
+    private final int number;
+    private final char letter;
 
-    protected Tile(String space) {
-        this.space = space;
+    /*
+    Constructor
+    Acquire tile pieces have a number and a letter to designate
+    where that tile can be placed on the board
+
+    @param number will be any number between 1 and 12 which correlates
+    to the columns of the gameboard.
+
+    @param letter will be a letter between A and I which correlates
+    to the rows of the gameboard.
+     */
+
+    protected Tile(int number, char letter){
+        this.number=number;
+        this.letter=letter;
+        space = String.valueOf(number) + letter;
     }
 
+    /*
+    method returns space
+    @return returns a string of the tile
+    i.e. 11B
+     */
     protected String getSpace() {
         return space;
+    }
+
+    /*
+    method @returns just the number value of the tile
+     */
+    public int getNumber(){
+        return number;
+    }
+    /*
+    method @returns just the letter value of the tile
+     */
+    public char getLetter(){
+        return letter;
     }
 }
