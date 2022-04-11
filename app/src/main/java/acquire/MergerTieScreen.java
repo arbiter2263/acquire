4345a861004ef5148e611941751ceb6ecb5127db
barkcolt <barkcolt@isu.edu>
2022-04-10 21:50:49 -0600

    Resolve merge conflicts

--- app/src/main/java/acquire/MergerTieScreen.java
+++ app/src/main/java/acquire/MergerTieScreen.java
@@ -5,44 +5,50 @@
 
 package acquire;
 
-import javafx.application.Application;
-import javafx.geometry.HPos;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-public class MergerTieScreen extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Merger");
-        GridPane gridPane = gameSetup();
-        addUIControls(gridPane);
-        Scene scene8 = new Scene(gridPane, 1200, 800);
-        ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 350, Double.MAX_VALUE);
-        columnOneConstraints.setHalignment(HPos.CENTER);
-        ColumnConstraints columnTwoConstraints = new ColumnConstraints(100, 350, Double.MAX_VALUE);
-        columnOneConstraints.setHalignment(HPos.CENTER);
-        ColumnConstraints columnThreeConstraints = new ColumnConstraints(100, 350, Double.MAX_VALUE);
-        columnOneConstraints.setHalignment(HPos.CENTER);
-        gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstraints, columnThreeConstraints);
-        primaryStage.setScene(scene8);
-        primaryStage.show();
-    }
+public class MergerTieScreen {
+    static Player user;
+    static String corporation1;
+    static String corporation2;
 
-    private GridPane gameSetup() {
+    protected Scene getScene(Stage primaryStage, Player e, String k, String f) throws FileNotFoundException {
         GridPane gridPane = new GridPane();
+        user = e;
+        Scene scene = new Scene(gridPane, 1200, 800);
+        gameSetup(gridPane);
+        addUIControls(primaryStage, gridPane);
+        return scene;
+    }
+/*
+    protected void loadScene(Stage primary, Player e, String k, String f) throws FileNotFoundException {
+        primary.setTitle("Merger Tie");
+        gameSetup();
+        addUIControls(gridPane);
+        user = e;
+        corporation1 = f;
+        corporation2 = k;
+        Scene scene14 = new Scene(gridPane, 1200, 800);
+        primary.setScene(scene14);
+    }
+*/
+    private GridPane gameSetup(GridPane gridPane) {
         gridPane.setAlignment(Pos.TOP_CENTER);
         gridPane.setHgap(10);
         gridPane.setVgap(10);
@@ -50,7 +56,7 @@ public class MergerTieScreen extends Application {
         return gridPane;
     }
 
-    private void addUIControls(GridPane gridPane) {
+    private void addUIControls(Stage primaryStage, GridPane gridPane) throws FileNotFoundException {
         TextArea merger = new TextArea("A merger has occurred between these 2 corporations: REPLACE ME 1 and REPLACE ME 2. The Corporations are the same size, select a corporation you would like to merge and take over. ");
         merger.setWrapText(true);
         merger.setPrefHeight(100);
@@ -58,21 +64,40 @@ public class MergerTieScreen extends Application {
         merger.setStyle("-fx-control-inner-background:#CBC3E3; -fx-font-size: 2em;");
         gridPane.add(merger, 0, 0, 3, 1);
 
-/* UNABLE TO FIND IMAGES
         // Add image of first corporation
-        FileInputStream inputstream = new FileInputStream("~/resources/filler.png");
+        FileInputStream inputstream = new FileInputStream(corporation1 + ".png");
         Image corpImage = new Image(inputstream);
         ImageView imageView = new ImageView(corpImage);
         gridPane.add(imageView, 0, 1);
+
         // Add image of second corporation
-        FileInputStream inputstream2 = new FileInputStream("~/resources/filler.png");
+        FileInputStream inputstream2 = new FileInputStream(corporation2 + ".png");
         Image corpImage2 = new Image(inputstream2);
         ImageView imageView2 = new ImageView(corpImage2);
         gridPane.add(imageView2, 2, 1);
-*/
-    }
 
-    public static void main(String[] args) {
-        launch();
+        Button corp1 = new Button(corporation1);
+        gridPane.add(corp1, 0, 2);
+
+        Button corp2 = new Button(corporation2);
+        gridPane.add(corp2, 2, 2);
+
+        corp1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Gameboard.getInstance().setCorpName(corp1.getText());
+                GameBoardScreen goBack = new GameBoardScreen();
+                primaryStage.setScene(goBack.getScene(primaryStage, user));
+            }
+        });
+
+        corp2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Gameboard.getInstance().setCorpName(corp1.getText());
+                GameBoardScreen goBack = new GameBoardScreen();
+                primaryStage.setScene(goBack.getScene(primaryStage, user));
+            }
+        });
     }
 }
