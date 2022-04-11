a9f16284f26e9bbd328d12918dbe2200312ff197
barkcolt <barkcolt@isu.edu>
2022-04-10 22:20:23 -0600

    Update UIHandler.java

--- app/src/main/java/acquire/UIHandler.java
+++ app/src/main/java/acquire/UIHandler.java
@@ -5,18 +5,9 @@
 
 package acquire;
 
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.util.ArrayList;
-
 public class UIHandler {
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    protected void s2GameSetup() {
+    public static void main(String[] args) {
+        MainScreen.main(args);
     }
 }
