06ced6620c38420ab6630cdec3c55285c192a8dc
barkcolt <barkcolt@isu.edu>
2022-04-10 22:17:35 -0600

    Update GameSetupScreen.java

--- app/src/main/java/acquire/GameSetupScreen.java
+++ app/src/main/java/acquire/GameSetupScreen.java
@@ -5,9 +5,9 @@
 
 package acquire;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.HPos;
+import com.sun.jdi.InterfaceType;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -16,35 +16,19 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class GameSetupScreen extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("New Game Setup");
-        GridPane gridPane = gameSetup();
-        addUIControls(gridPane);
-        Scene scene2 = new Scene(gridPane, 1200, 800);
-        ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 350, Double.MAX_VALUE);
-        columnOneConstraints.setHalignment(HPos.CENTER);
-        ColumnConstraints columnTwoConstraints = new ColumnConstraints(100, 350, Double.MAX_VALUE);
-        columnOneConstraints.setHalignment(HPos.CENTER);
-        ColumnConstraints columnThreeConstraints = new ColumnConstraints(100, 350, Double.MAX_VALUE);
-        columnOneConstraints.setHalignment(HPos.CENTER);
-        gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstraints, columnThreeConstraints);
-        primaryStage.setScene(scene2);
-        primaryStage.show();
-    }
+import java.util.LinkedList;
 
-    private GridPane gameSetup() {
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
+public class GameSetupScreen {
+    private GridPane gameSetup(GridPane gridPane) {
+        gridPane.setAlignment(Pos.TOP_CENTER);
         gridPane.setBackground(new Background(new BackgroundFill(Color.web("#CBC3E3"), CornerRadii.EMPTY, Insets.EMPTY)));
         gridPane.setHgap(10);
         gridPane.setVgap(10);
         return gridPane;
     }
 
-    private void addUIControls(GridPane gridPane) {
-        // Add Player Count Button
+    protected void addUIControls(Stage primaryStage, GridPane gridPane) {
+        // Add Player Count Choicebox
         ChoiceBox<String> playerCount = new ChoiceBox<>();
         playerCount.getItems().addAll("2", "3", "4", "5", "6");
         playerCount.setValue("How many players?");
@@ -53,7 +37,7 @@ public class GameSetupScreen extends Application {
         gridPane.add(playerCount, 0, 0);
         GridPane.setMargin(playerCount, new Insets(20, 0,20,0));
 
-        // Add Advanced Mode Button
+        // Add Advanced Mode Checkbox
         CheckBox advanced = new CheckBox("Play in Advanced* mode?");
         advanced.setPrefHeight(50);
         advanced.setPrefWidth(200);
@@ -75,8 +59,36 @@ public class GameSetupScreen extends Application {
         startButton.setStyle("-fx-background-color:#90ee90; -fx-border-color:#000000");
         gridPane.add(startButton, 2, 3);
         GridPane.setMargin(startButton, new Insets(20, 0,20,0));
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                GameSystem.getInstance().initializeGame(advanced.isSelected(), Integer.parseInt(playerCount.getValue()));
+                LinkedList<Player> players = Gameboard.getInstance().getPlayers();
+                for (int i=0; i<players.size();) {
+                    GameSystem.getInstance().playOrder(players.remove(i));
+                }
+                GameBoardScreen gbs = new GameBoardScreen();
+                primaryStage.setScene(gbs.getScene(primaryStage, GameSystem.getInstance().playerTurn()));
+            }
+        });
     }
-    public static void main() {
-        launch();
+
+    protected Scene getScene(Stage primaryStage) {
+        GridPane gridPane = new GridPane();
+        Scene scene = new Scene(gridPane, 1200, 800);
+        gameSetup(gridPane);
+        addUIControls(primaryStage, gridPane);
+        return scene;
     }
+/*
+    protected void loadScene() {
+        Scene scene = new Scene(gridPane, 1200, 800);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        gameSetup();
+        //addUIControls();
+        stage.show();
+    }
+*/
 }
