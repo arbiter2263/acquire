1c5038c79ebf2ed844c3f3bc15062bcf941b470f
barkcolt <barkcolt@isu.edu>
2022-04-10 21:47:36 -0600

    Resolving merge conflicts

--- app/src/main/java/acquire/DefunctStockOptionsScreen.java
+++ app/src/main/java/acquire/DefunctStockOptionsScreen.java
@@ -5,5 +5,105 @@
 
 package acquire;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class DefunctStockOptionsScreen {
+    static GridPane gridPane = new GridPane();
+    static String corporation;
+    static Player user;
+
+    protected void loadScene(Stage primary, Player e, String corp) throws FileNotFoundException {
+        user = e;
+        corporation = corp;
+        primary.setTitle("Buy Stock");
+        stockSell(gridPane);
+        Scene scene13 = new Scene(gridPane, 1200, 800);
+        addUIControls(gridPane);
+        primary.setScene(scene13);
+    }
+
+    private GridPane stockSell(GridPane gridPane) {
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setBackground(new Background(new BackgroundFill(Color.web("#CBC3E3"), CornerRadii.EMPTY, Insets.EMPTY)));
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        return gridPane;
+    }
+
+    private void addUIControls(GridPane gridPane) throws FileNotFoundException {
+        // Add Header
+        Label headerLabel = new Label("Corporation " + corporation + " has gone defunct. What would you like to do with the stock you own in it?");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        gridPane.add(headerLabel, 0,0,3,1);
+        GridPane.setHalignment(headerLabel, HPos.CENTER);
+        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
+
+
+        // Add Sell Button
+        Button sellButton = new Button("Sell");
+        sellButton.setPrefHeight(100);
+        sellButton.setDefaultButton(true);
+        sellButton.setPrefWidth(300);
+        sellButton.setStyle("-fx-background-color:#ADD8E6; -fx-border-color:#000000");
+        gridPane.add(sellButton, 0, 3, 1, 1);
+        GridPane.setMargin(sellButton, new Insets(20, 0,20,0));
+
+        sellButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+        // Add Trade Button
+        Button tradeButton = new Button("Trade");
+        tradeButton.setPrefHeight(100);
+        tradeButton.setDefaultButton(false);
+        tradeButton.setPrefWidth(300);
+        tradeButton.setStyle("-fx-background-color:#ADD8E6; -fx-border-color:#000000");
+        gridPane.add(tradeButton, 1, 3, 1, 1);
+        GridPane.setMargin(tradeButton, new Insets(20, 0,20,0));
+
+        tradeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+        // Add Hold Button
+        Button holdButton = new Button("Hold");
+        holdButton.setPrefHeight(100);
+        holdButton.setDefaultButton(false);
+        holdButton.setPrefWidth(300);
+        holdButton.setStyle("-fx-background-color:#ADD8E6; -fx-border-color:#000000");
+        gridPane.add(holdButton, 2, 3, 1, 1);
+        GridPane.setMargin(holdButton, new Insets(20, 0,20,0));
+
+        holdButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Alert confirm = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to hold stock in the defunct corporation? This will end selling and trading in stock.");
+                confirm.showAndWait();
+                if (confirm.getResult() == ButtonType.YES) {}
+            }
+        });
+    }
+
 }
